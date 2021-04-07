55bbf9ed555b613f35174cce1e06b856df5944cd
Max <rekin.m@gmail.com>
2021-04-07 11:47:30 +0300

    Initial commit

---
+++ dockerfile
@@ -0,0 +1,35 @@
+FROM debian:buster-slim
+RUN apt-get update -y; 
+RUN apt-get upgrade -y;
+#RUN apt-get install -y python3.8;
+#RUN pip3 install pip;
+
+# gcc compiler and opencv prerequisites
+RUN apt-get -y install nano git build-essential libglib2.0-0 libsm6 libxext6 libxrender-dev python3 python3-pip python3-opencv wget
+
+
+RUN python3 --version;
+#RUN apt-get install -y python3-pip
+#RUN pip3 install pip;
+RUN pip3 install --upgrade pip
+RUN pip install -U numpy
+
+# Detectron2 prerequisites
+RUN pip install -U torch==1.8.1+cpu torchvision==0.9.1+cpu -f https://download.pytorch.org/whl/torch_stable.html
+
+# Detectron2 - CPU
+RUN pip install -U detectron2 -f https://dl.fbaipublicfiles.com/detectron2/wheels/cpu/torch1.8/index.html
+RUN pip install -U cython flask flask-cors==3.0.9 requests opencv-python \
+	Image \
+	piexif \
+;
+RUN pip install -U 'git+https://github.com/cocodataset/cocoapi.git#subdirectory=PythonAPI'
+RUN pip install -U cython requests opencv-python pyyaml==5.1
+# Revert back pycocotools==2.0.2 - need to check why it is moved to 2.0
+RUN pip install -U pycocotools==2.0.2
+
+# Python flask service
+RUN mkdir /home/detecs
+ADD service.py /home/detecs/
+
+CMD python3 /home/detecs/service.py
---
+++ service.py
@@ -0,0 +1,221 @@
+import torch, torchvision
+print(torch.__version__, torch.cuda.is_available())
+import torch
+assert torch.__version__.startswith("1.8")   # need to manually install torch 1.8 if Colab changes its default version
+
+# import some common libraries
+import numpy as np
+import os, json, cv2, random, io
+
+import detectron2
+# import some common detectron2 utilities
+from detectron2 import model_zoo
+from detectron2.engine import DefaultPredictor
+from detectron2.config import get_cfg
+from detectron2.utils.visualizer import Visualizer
+from detectron2.data import MetadataCatalog, DatasetCatalog
+
+from detectron2.utils.logger import setup_logger
+setup_logger()
+
+from flask import Flask, jsonify, abort,render_template,request,redirect,url_for
+
+from werkzeug.utils import secure_filename
+from PIL import Image
+from PIL.ExifTags import TAGS
+import piexif
+
+app = Flask(__name__)
+predictor = None
+segmodel = 'COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml'
+
+def _create_text_labels(classes, scores, class_names, is_crowd=None):
+    """
+    Args:
+        classes (list[int] or None):
+        scores (list[float] or None):
+        class_names (list[str] or None):
+        is_crowd (list[bool] or None):
+
+    Returns:
+        list[str] or None
+    """
+    labels = None
+    if classes is not None:
+        if class_names is not None and len(class_names) > 0:
+            labels = [class_names[i] for i in classes]
+        else:
+            labels = [str(i) for i in classes]
+    if scores is not None:
+        if labels is None:
+            labels = ["{:.0f}%".format(s * 100) for s in scores]
+        else:
+            labels = ["{} {:.0f}%".format(l, s * 100) for l, s in zip(labels, scores)]
+    if labels is not None and is_crowd is not None:
+        labels = [l + ("|crowd" if crowd else "") for l, crowd in zip(labels, is_crowd)]
+    return labels
+
+def _create_names_dict(classes, scores, class_names, is_crowd=None):
+    """
+    Args:
+        classes (list[int] or None):
+        scores (list[float] or None):
+        class_names (list[str] or None):
+        is_crowd (list[bool] or None):
+
+    Returns:
+        list[str] or None
+    """
+    dict = []
+    labels = None
+    if classes is not None:
+        if class_names is not None and len(class_names) > 0:
+            labels = [class_names[i] for i in classes]
+        else:
+            labels = [str(i) for i in classes]
+    for i, (l, s) in enumerate(zip(labels, scores)):
+#        dict.append([l,"{:.2f}".format(s * 100)])
+        dict.append(
+            {
+                "name" : l, 
+                "score" : "{:.2f}".format(s * 100)
+            })
+
+    return dict
+
+
+def preparePredictor():
+    print('Prepearing predictor...')
+    cfg = get_cfg()
+    # add project-specific config (e.g., TensorMask) here if you're not running a model in detectron2's core library
+    
+    print('Using model ' + segmodel)
+    print('You can find more models in model-zoo: https://github.com/facebookresearch/detectron2/tree/master/configs/COCO-InstanceSegmentation')
+    cfg.merge_from_file(model_zoo.get_config_file(segmodel))
+    cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5  # set threshold for this model
+    # Find a model from detectron2's model zoo. You can use the https://dl.fbaipublicfiles... url as well
+    cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url(segmodel)
+    cfg.MODEL.DEVICE='cpu'
+    predictor = DefaultPredictor(cfg)
+    print('Predictor ready!')
+    return predictor
+
+
+def analizeImg (image):
+    global predictor
+    if predictor is None:
+        predictor =  preparePredictor()
+
+#predictor(cv2.cvtColor(np.array(image), cv2.COLOR_BGR2RGB))
+    image = cv2.cvtColor(np.array(image), cv2.COLOR_BGR2RGB)
+    outputs = predictor(image)
+
+    metadata = MetadataCatalog.get("coco_2017_val")
+    names = metadata.get("thing_classes", None)
+    classes = outputs["instances"].pred_classes
+    scores = outputs["instances"].scores
+
+    label2 = _create_names_dict(classes,scores,names,None)
+    print(label2)
+
+    return label2
+
+def getLabesShortList(labels:{}):
+    res = list(dict.fromkeys([ e['name'] for e in labels ]))
+    return res
+
+def getExif(image:Image):
+    exif = image.getexif()
+
+    labeled = {}
+    for (key, val) in exif.items():
+        if TAGS.get(key) is not None:
+            labeled[TAGS.get(key)] = str(val)
+
+    return labeled
+
+
+
+def getExif2(image:Image):
+    tags = {}
+    exif_dict = piexif.load(image.info["exif"])
+    for ifd in ("0th", "Exif", "GPS", "1st"):
+        for tag in exif_dict[ifd]:
+            if piexif.TAGS[ifd][tag]["name"] is not None:
+                if isinstance(exif_dict[ifd][tag], (bytes)):
+                    try: 
+                        val = exif_dict[ifd][tag].decode('utf-8')
+                    except (UnicodeDecodeError, AttributeError):
+                        val = exif_dict[ifd][tag].decode('unicode-escape')
+                        pass
+                else:
+                    val = exif_dict[ifd][tag]
+                tags[piexif.TAGS[ifd][tag]["name"]] = val
+    if 'XPKeywords' in tags:
+        keyword = tags['XPKeywords']
+        tags['XPKeywords'] = decodeXP(keyword)
+
+    if 'XPComment' in tags:
+        keyword = tags['XPComment']
+        tags['XPComment'] = decodeXP(keyword)
+    return tags
+
+def decodeXP(t):
+    """
+    Takes a exif XPKeywords tag and decodes it to a text string
+    """
+    b = bytes(t)
+    return b[:-2].decode('utf-16-le')
+
+
+
+#############################################################################
+###                WebService methods                                     ###
+#############################################################################
+@app.route('/')
+def index():
+    return "Hello, World!"
+
+# 
+@app.route('/api/v1.0/imgrecognize/', methods=['POST'])
+def upload_file():
+    resp ={}
+    if request.method =='POST':
+        print (request.files.keys)
+        for fn in request.files:
+            file = request.files[fn]
+            
+            if file:
+                filename = secure_filename(file.filename)
+                resp[filename] = {}
+                img = Image.open(file)
+                resp[filename]['objects'] = analizeImg(img)
+                resp[filename]['objectsShortList'] = getLabesShortList(resp[filename]['objects'])
+                resp[filename]['exif'] = getExif2(img)
+#               img = cv2.imdecode(img, cv2.IMREAD_COLOR)
+                
+    return jsonify(resp)
+
+
+
+
+if __name__ == '__main__':
+
+  #  global predictor 
+    if 'SEGMENTATION_MODEL' in os.environ:
+        segmodel = os.environ['SEGMENTATION_MODEL']
+    if 'FLASK_DEBUG' in os.environ:
+        fl_debug = os.environ['FLASK_DEBUG']
+    else:
+        fl_debug = False
+    if 'FLASK_HOST' in os.environ:
+        fl_host = os.environ['FLASK_HOST']
+    else:
+        fl_host = '0.0.0.0'
+    if 'FLASK_PORT' in os.environ:
+        fl_port = os.environ['FLASK_PORT']
+    else:
+        fl_port = 5000
+    predictor =  preparePredictor()
+    app.debug=True
+    app.run(host='0.0.0.0',port=5000)
